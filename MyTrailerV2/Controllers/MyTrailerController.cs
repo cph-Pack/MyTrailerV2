8c4bf7e4cb0c8fb9383a35ec1640b821a00a6bc3
Dieky <36627007+Dieky@users.noreply.github.com>
2024-10-12 10:10:45 +0200

    added base classes and controller

---
+++ MyTrailerV2/Controllers/MyTrailerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTrailerV2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyTrailerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MyTrailerV2/Data/Trailer.cs
+++ MyTrailerV2/Data/Trailer.cs
@@ -1,6 +1,40 @@
-﻿namespace MyTrailerV2.Data
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MyTrailerV2.Data
 {
     public class Trailer
     {
+        [BsonId]
+        public ObjectId Id { get; private set; }
+        public int TrailerNumber { get; private set; }
+        public bool IsAvailable { get; private set; } = true;
+        public DateTime? RentedUntil { get; private set; }
+
+        public Trailer(int trailerNumber)
+        {
+            TrailerNumber = trailerNumber;
+            RentedUntil = DateTime.Today.AddDays(1);
+        }
+
+        public void RentTrailer(DateTime endTime)
+        {
+            if (!IsAvailable)
+                throw new InvalidOperationException("Trailer is already rented.");
+
+            IsAvailable = false;
+            RentedUntil = endTime;
+        }
+
+        public void ReturnTrailer()
+        {
+            IsAvailable = true;
+            RentedUntil = null;
+        }
+
+        public bool IsLateReturn()
+        {
+            return RentedUntil.HasValue && RentedUntil.Value < DateTime.Now;
+        }
     }
 }
